90e118295259fd367eb2c69254c9f6d1a46e31df
Jake Stannix <jakestannix@gmail.com>
2023-08-11 01:48:48 -0600

    parser outlined

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -11,70 +11,23 @@ use precedence::*;
 use crate::{byte_code::OpCode, lexer::TokenType, value::Value};
 
 fn parse_precedence<'a>(parser: &mut Parser<'a>, prec: Precedence) -> Result<(), CompilerError> {
-    parser.advance()?;
-    let Some(prefix_rule) = parser.previous.id.get_rule().map(|x| x.prefix).flatten() else {
-	return Err(parser.error("Expect expression."));
-    };
-    prefix_rule(parser)?;
-
-    while Some(prec)
-        <= parser
-            .peek_current()
-            .map(|x| x.id.get_rule().map(|x| x.precedence))
-            .flatten()
-    {
-        parser.advance()?;
-
-        if let Some(infix_rule) = parser.previous.id.get_rule().map(|x| x.infix).flatten() {
-            infix_rule(parser)?;
-        }
-    }
-
-    Ok(())
+todo!()
 }
 fn expression<'a>(parser: &mut Parser<'a>) -> Result<(), CompilerError> {
-    parse_precedence(parser, Precedence::Assignment)
+todo!()
 }
 fn number<'a>(parser: &mut Parser<'a>) -> Result<(), CompilerError> {
-    let num = parser.previous.lexum.parse::<i64>().unwrap();
-    parser.emit_byte(OpCode::Constant(num.into()));
-    Ok(())
+    todo!()
 }
 fn grouping<'a>(parser: &mut Parser<'a>) -> Result<(), CompilerError> {
-    expression(parser)?;
-    parser.advance_if(TokenType::RightParen, "Expect ')' after expression.")
+    todo!()
 }
 fn unary<'a>(parser: &mut Parser<'a>) -> Result<(), CompilerError> {
-    let op_type = parser.previous.id;
-
-    parse_precedence(parser, Precedence::Unary)?;
-
-    match op_type {
-        TokenType::Minus => parser.emit_byte(OpCode::Neg),
-        _ => unreachable!(),
-    }
-    Ok(())
+    todo!()
 }
 fn literal<'a>(parser: &mut Parser<'a>) -> Result<(), CompilerError> {
-    match parser.previous.id {
-	TokenType::False => parser.emit_byte(OpCode::False),
-	TokenType::True => parser.emit_byte(OpCode::True),
-	TokenType::Nil => parser.emit_byte(OpCode::Nil),
-	_ => unreachable!(),
-	
-    }
-    Ok(())
+    todo!()
 }
 fn binary<'a>(parser: &mut Parser<'a>) -> Result<(), CompilerError> {
-    let op_type = parser.previous.id;
-    let rule = op_type.get_rule().unwrap().precedence;
-    parse_precedence(parser, rule + 1)?;
-    match op_type {
-        TokenType::Plus => parser.emit_byte(OpCode::Add),
-        TokenType::Minus => parser.emit_byte(OpCode::Sub),
-        TokenType::Star => parser.emit_byte(OpCode::Mul),
-        TokenType::Slash => parser.emit_byte(OpCode::Div),
-        _ => unreachable!(),
-    }
-    Ok(())
+    todo!()
 }
--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -8,97 +8,95 @@ use crate::{
 use super::{expression, CompilerError};
 
 pub(crate) struct Parser<'a> {
-    pub(super) previous: Token<'a>,
-    pub(super) lexer: Peekable<Lexer<'a>>,
-    pub(super) que: VecDeque<Result<(OpCode, usize), CompilerError>>,
+    previous: Option<Token<'a>>,
+    current: Option<Token<'a>>,
+    lexer: Peekable<Lexer<'a>>,
+    que: VecDeque<Result<(OpCode, usize), CompilerError>>,
 }
 
-impl<'a> Parser<'a> {
-    pub(crate) fn new(lexer: Lexer<'a>) -> Self {
+impl<'a> Iterator for Parser<'a> {
+    type Item = Result<(OpCode, usize), CompilerError>;
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!("implement this")
+    }
+}
+
+impl<'a> Default for Parser<'a> {
+    fn default() -> Self {
         Self {
-            previous: Token::default(),
-            lexer: lexer.peekable(),
+            previous: None,
+            current: None,
+            lexer: Lexer::new("").peekable(),
             que: VecDeque::new(),
         }
     }
-    pub(super) fn advance(&mut self) -> Result<(), CompilerError> {
-        match self.lexer.next() {
-            Some(Ok(token)) => {
-                self.previous = token;
-            }
-            Some(Err(err)) => return Err(err.into()),
-            None => self.previous = Token::default(),
-        }
-        Ok(())
-    }
-    pub(super) fn advance_if(
-        &mut self,
-        pred: TokenType,
-        message: impl ToString,
-    ) -> Result<(), CompilerError> {
-        match self.lexer.peek() {
-            Some(Ok(t)) => {
-                if t.id == pred {
-                    return self.advance();
-                }
-                Err(self.error_at_current(message))
-            }
-            None => {
-                if pred == TokenType::None {
-                    return Ok(());
-                }
-                Err(self.error_at_current(message))
-            }
-            _ => Ok(()),
-        }
-    }
-    pub(super) fn peek_current(&mut self) -> Option<&Token<'a>> {
-        match self.lexer.peek() {
-            Some(Ok(t)) => Some(t),
-            _ => None,
-        }
-    }
-    pub(super) fn error_at_current(&mut self, message: impl ToString) -> CompilerError {
-        match self.lexer.peek() {
-            Some(Err(err)) => err.clone().into(),
-            Some(Ok(err)) => CompilerError::new(false, *err, message),
-            None => CompilerError::new(true, Token::default(), message),
-        }
-    }
-    pub(super) fn error(&mut self, message: impl ToString) -> CompilerError {
-        CompilerError::new(self.is_at_end(), self.previous, message)
-    }
-    fn is_at_end(&mut self) -> bool {
-        self.lexer.peek().is_none() && self.previous.id == TokenType::None
-    }
-    pub(super) fn emit_byte(&mut self, op_code: OpCode) {
-        let line = self.previous.line;
-        self.que.push_back(Ok((op_code, line)));
-    }
-    pub(super) fn end_compiler(&mut self) {
-        self.emit_return();
-    }
-    pub(super) fn emit_return(&mut self) {
-        self.emit_byte(OpCode::Return);
-    }
 }
 
-impl<'a> Iterator for Parser<'a>
-where
-    Self: 'a,
-{
-    type Item = Result<(OpCode, usize), CompilerError>;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.que.is_empty() && self.lexer.peek().is_some() {
-            match expression(self) {
-                Ok(()) => (),
-                Err(err) => return Some(Err(err)),
-            }
-            if let Err(err) = self.advance_if(TokenType::None, "Expected end of expression") {
-                return Some(Err(err));
-            }
-            self.end_compiler();
+impl<'a> Parser<'a> {
+    pub(crate) fn new(source: &'a str) -> Self {
+        Self {
+            lexer: Lexer::new(source).peekable(),
+            ..Default::default()
         }
-        self.que.pop_front()
+    }
+    pub(crate) fn map_previous<T: FnOnce(&Token<'a>) -> U, U>(&self, func: T) -> Option<U> {
+        self.previous.as_ref().map(func)
+    }
+    pub(crate) fn map_current<T: FnOnce(&Token<'a>) -> U, U>(&self, func: T) -> Option<U> {
+        self.current.as_ref().map(func)
+    }
+    pub(crate) fn is_previous<T: FnOnce(&Token<'a>) -> bool>(&self, func: T) -> bool {
+        self.previous.as_ref().map_or(false, func)
+    }
+    pub(crate) fn is_current<T: FnOnce(&Token<'a>) -> bool>(&self, func: T) -> bool {
+        self.current.as_ref().map_or(false, func)
+    }
+    pub(crate) fn is_at_end(&mut self) -> bool {
+        self.lexer.peek().is_none()
+    }
+    pub(crate) fn advance(&mut self) -> Result<Option<Token<'a>>, CompilerError> {
+        self.previous = self.current;
+        let x = match self.lexer.next() {
+            Some(Ok(t)) => Some(t),
+            Some(Err(err)) => return Err(err.into()),
+            None => None,
+        };
+        self.current = x;
+        Ok(self.current)
+    }
+    pub(crate) fn advance_if<T: FnOnce(&Token<'a>) -> bool>(
+        &mut self,
+        func: T,
+    ) -> Result<Option<Token<'a>>, CompilerError> {
+        if self.is_current(func) {
+            self.advance()
+        } else {
+            Ok(None)
+        }
+    }
+    pub(crate) fn advance_if_id(
+        &mut self,
+        id: TokenType,
+        message: impl ToString,
+    ) -> Result<Option<Token<'a>>, CompilerError> {
+        if self.is_current(|t| t.id == id) {
+            self.advance()
+        } else {
+            Err(CompilerError::new(
+                self.is_at_end(),
+                self.previous.unwrap_or_default(),
+                message,
+            ))
+        }
+    }
+    pub(crate) fn advance_if_at_end(
+        &mut self,
+        message: impl ToString,
+    ) -> Result<(), CompilerError> {
+        if self.is_at_end() {
+            Ok(())
+        } else {
+            Err(CompilerError::new(false, self.current.unwrap(), message))
+        }
     }
 }
